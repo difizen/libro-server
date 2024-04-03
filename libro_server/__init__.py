26d74a75ce58267a8bf9c4df4845404819593f66
zhanba <c5e1856@gmail.com>
2024-04-03 14:34:18 +0800

    chore: seperate libro-ai

--- libro_server/__init__.py
+++ libro_server/__init__.py
@@ -2,14 +2,6 @@ from ._version import __version__
 
 from .app import LibroApp
 
-from .magics import load_ipython_extension, unload_ipython_extension
-
-from .flow import define_nb_args,execute_notebook,to_nb_output,load_nb_output
-
-from .chat import chat_provider, ChatProvider, ChatExecutor, ChatItem, ChatItemProvider
-
-from .utils import is_ipython, is_langchain_installed,  get_variable_list, get_variable_dict_list, get_langchain_variable_dict_list
-
 def _jupyter_server_extension_points():
     """
     Returns a list of dictionaries with metadata describing
