15645b2f8e6344fe7b7e0560af7d0cf30e580c4f
brokun <brokun0128@gmail.com>
2024-05-11 15:50:23 +0800

    feat: add execute_notebook_sync method

--- libro-flow/src/libro_flow/__init__.py
+++ libro-flow/src/libro_flow/__init__.py
@@ -3,6 +3,7 @@ from .libro_execution import (
     dump_execution_result,
     load_execution_result,
     execute_notebook,
+    execute_notebook_sync,
     inspect_execution_result,
 )
 from .libro_client import LibroNotebookClient
