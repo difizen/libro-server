ee6a39dff216b20532397e7f4d568ad32ebea33c
brokun <brokun0128@gmail.com>
2024-09-14 02:44:53 +0800

    feat(sql): sql magic, support postgresql/mysql/sqlite

---
+++ libro-sql/src/libro_sql/extensions.py
@@ -0,0 +1,22 @@
+from IPython.core.interactiveshell import InteractiveShell
+from .exception import store_exception
+from .sql_magic import SQLMagic
+
+
+def load_ipython_extension(ipython: InteractiveShell):
+    ipython.register_magics(SQLMagic)
+    ipython.set_custom_exc((BaseException,), store_exception)
+
+
+def unload_ipython_extension(ipython: InteractiveShell):
+    ipython.set_custom_exc((BaseException,), ipython.CustomTB)
+
+
+def _load_jupyter_server_extension(ipython):
+    """Load the Jupyter server extension.
+    Parameters
+    ----------
+    ipython: :class:`jupyter_client.ioloop.IOLoopKernelManager`
+        Jupyter kernel manager instance.
+    """
+    load_ipython_extension(ipython)
