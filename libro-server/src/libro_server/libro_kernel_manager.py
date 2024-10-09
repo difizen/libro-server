1dcb819aafed6a101253c25f3e4fa9f400dab302
sunshinesmilelk <1176136681@qq.com>
2024-10-10 02:48:24 +0800

    fix: use libro config for load jpserver_extensions when restart kernel

--- libro-server/src/libro_server/libro_kernel_manager.py
+++ libro-server/src/libro_server/libro_kernel_manager.py
@@ -20,14 +20,41 @@ class LibroKernelManager(ServerKernelManager):
         self.log.debug("Init execution codes: %r", code)
 
         client = self.blocking_client()
-        client.execute
         try:
             client.start_channels()
             client.wait_for_ready(30)
             client.execute(code)
+
         except Exception as e:
             logger.warn("failed to run code with kernel, exp: %s" % e)
         finally:
             if client:
                 client.stop_channels()
         return kernel_id
+
+    async def restart_kernel(self, *args, **kwargs):
+        await super(LibroKernelManager,self).restart_kernel(*args, **kwargs)
+        # 在内核启动时执行特定代码
+        extensions = libro_config.get_config().get("jpserver_extensions",{})
+        commands = []
+
+        # 遍历扩展，生成相应的 %load_ext 或 %unload_ext 命令
+        for ext, enabled in extensions.items():
+            command = f"%load_ext {ext}" if enabled else f"%unload_ext {ext}"
+            commands.append(command)
+
+        # 使用 join 方法将所有命令连接成一个字符串
+        code = "\n".join(commands)
+        self.log.debug("Init execution codes: %r", code)
+
+        client = self.blocking_client()
+        try:
+            client.start_channels()
+            client.wait_for_ready(30)
+            client.execute(code)
+
+        except Exception as e:
+            logger.warn("failed to run code with kernel, exp: %s" % e)
+        finally:
+            if client:
+                client.stop_channels()
