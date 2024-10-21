2ceec1ec8b2fb1ec38b162b2d4b0e4013adf751c
sunshinesmilelk <ximo.lk@antgroup.com>
2024-10-21 13:05:37 +0800

    feat(ai): add LibroChatStreamHandler

---
+++ lab/src/pages/libro/test-command.tsx
@@ -0,0 +1,9 @@
+export const LibroTestCommand = {
+    TestCommand: {
+      id: 'libro-test:command',
+    },
+    // LibroSearchToggle: {
+    //   id: 'libro-search:toogle',
+    //   keybind: 'ctrlcmd+F',
+    // },
+  };
