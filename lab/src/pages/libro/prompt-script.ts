7cd985bd004deeda9ab25666142be81aa7cc6380
sunshinesmilelk <ximo.lk@antgroup.com>
2024-11-01 11:28:01 +0800

    chore: update version & update prompt cell script

--- lab/src/pages/libro/prompt-script.ts
+++ lab/src/pages/libro/prompt-script.ts
@@ -4,7 +4,7 @@ import { PromptScript } from '@difizen/libro-prompt-cell';
 @singleton()
 export class LibroPromptScript extends PromptScript {
   public override readonly getChatObjects = `from libro_ai import chat_object_manager
-chat_object_manager.dump_list_json()` as any;
+chat_object_manager.dump_kernel_list_json()` as any;
   public override readonly getChatRecoreds = `from libro_ai import chat_record_provider
 chat_record_provider.get_records()` as any;
 }
