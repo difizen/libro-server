67bc3edc5c127c692d572533ad38b2bc8b911264
sunshinesmilelk <ximo.lk@antgroup.com>
2024-11-11 11:27:06 +0800

    feat: dbgpt adaptable

--- lab/src/pages/dbgpt/app.ts
+++ lab/src/pages/dbgpt/app.ts
@@ -1,14 +1,20 @@
 import {
   FileCommandContribution,
+  LibroFileService,
   LibroJupyterConfiguration,
   PageConfig,
   ServerConnection,
   ServerManager,
 } from '@difizen/libro-jupyter';
-import { ConfigurationService } from '@difizen/mana-app';
+import { ConfigurationService, FileTreeView, FileTreeViewFactory, OpenerService, URI } from '@difizen/mana-app';
 import { SlotViewManager } from '@difizen/mana-app';
+import { terminalDefaultSlot } from '@difizen/libro-terminal';
+import qs from 'query-string';
 import { ApplicationContribution, ViewManager } from '@difizen/mana-app';
 import { inject, singleton } from '@difizen/mana-app';
+import { Fetcher } from '@difizen/magent-core';
+import { LayoutService, LibroLabLayoutSlots } from '@difizen/libro-lab';
+const ShouldPreventStoreViewKey = 'mana-should-prevent-store-view';
 
 @singleton({ contrib: ApplicationContribution })
 export class LibroApp implements ApplicationContribution {
@@ -17,7 +23,12 @@ export class LibroApp implements ApplicationContribution {
   @inject(ViewManager) viewManager: ViewManager;
   @inject(SlotViewManager) slotViewManager: SlotViewManager;
   @inject(ConfigurationService) configurationService: ConfigurationService;
+  @inject(LayoutService) layoutService: LayoutService;
   @inject(FileCommandContribution) fileCommandContribution: FileCommandContribution;
+  @inject(Fetcher) fetcher: Fetcher;
+  @inject(OpenerService) openerService: OpenerService;
+  @inject(LibroFileService) libroFileService: LibroFileService;
+  location: string
 
   async onStart() {
     this.configurationService.set(LibroJupyterConfiguration.AllowDownload, true);
@@ -33,10 +44,59 @@ export class LibroApp implements ApplicationContribution {
         baseUrl = window.location.origin + baseUrl;
       }
     }
+    localStorage.setItem(ShouldPreventStoreViewKey, 'true');
+    this.configurationService.set(
+      LibroJupyterConfiguration['OpenSlot'],
+      LibroLabLayoutSlots.content,
+    );
+    this.configurationService.set(
+      terminalDefaultSlot,
+      LibroLabLayoutSlots.contentBottom,
+    );
     this.serverConnection.updateSettings({
       baseUrl,
       wsUrl: baseUrl.replace(/^http(s)?/, 'ws$1'),
     });
     this.serverManager.launch();
+    this.serverManager.ready
+      .then(async () => {
+        this.layoutService.setAreaVisible(LibroLabLayoutSlots.navigator, true);
+        this.layoutService.setAreaVisible(LibroLabLayoutSlots.alert, false);
+        this.layoutService.serverSatus = 'success';
+        await this.initialWorkspace();
+        if(this.location){
+          const defaultOpenUri = new URI(this.location+'/__init__.py');
+
+          this.openerService.getOpener(defaultOpenUri).then((opener) => {
+            if (opener) {
+              opener.open(defaultOpenUri, {
+                viewOptions: {
+                  name: '__init__.py',
+                },
+              });
+            }
+          });
+        }
+        return;
+      })
+      .catch(console.error);
+  }
+
+  protected async initialWorkspace() {    
+    const queryParams = qs.parse(window.location.search);
+    const flow_uid = queryParams['flow_uid'];
+    const res = await this.fetcher.get<any>(`/api/v1/serve/awel/flow/notebook/file/path`, {
+      flow_uid:flow_uid,
+    },{baseURL:'http://localhost:5670'});
+    if(res.status&& res.data?.data?.path){
+      const view =
+      await this.viewManager.getOrCreateView<FileTreeView>(FileTreeViewFactory);
+      if (view) {
+        const location = res.data?.data?.path;
+        this.location = location;
+        view.model.rootVisible = false;
+        view.model.location = new URI(location);
+      }
+    }
   }
 }
