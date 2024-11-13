c93967b0f32aa0447742f919e6b9e883e146a511
sunshinesmilelk <ximo.lk@antgroup.com>
2024-11-13 11:18:47 +0800

    fix: add compatibility logic for jpserver_extensions in libro_config

--- libro-server/src/libro_server/libro_kernel_manager.py
+++ libro-server/src/libro_server/libro_kernel_manager.py
@@ -6,10 +6,15 @@ logger = logging.getLogger(__name__)
 
 class LibroKernelManager(ServerKernelManager):
 
-    async def start_kernel(self, **kw):
-        kernel_id = await super(LibroKernelManager, self).start_kernel(**kw)
+    async def start_kernel(self,**kw):
+        from packaging import version
+        kernel_id = await super(LibroKernelManager,self).start_kernel(**kw)
         # 在内核启动时执行特定代码
-        extensions = libro_config.get_config().get("ipython_extensions", {})
+        cur_version = libro_config.get_config().get('version')
+        if cur_version is None or version.parse(cur_version) < version.parse("0.0.2"):
+            if "jpserver_extensions" in libro_config.config:
+                libro_config.config['ipython_extensions'] = libro_config.config.pop("jpserver_extensions")
+        extensions = libro_config.get_config().get("ipython_extensions",{})
         commands = []
 
         # 遍历扩展，生成相应的 %load_ext 或 %unload_ext 命令
@@ -35,9 +40,14 @@ class LibroKernelManager(ServerKernelManager):
         return kernel_id
 
     async def restart_kernel(self, *args, **kwargs):
-        await super(LibroKernelManager, self).restart_kernel(*args, **kwargs)
+        from packaging import version
+        await super(LibroKernelManager,self).restart_kernel(*args, **kwargs)
         # 在内核启动时执行特定代码
-        extensions = libro_config.get_config().get("ipython_extensions", {})
+        cur_version = libro_config.get_config().get('version')
+        if cur_version is None or version.parse(cur_version) < version.parse("0.0.2"):
+            if "jpserver_extensions" in libro_config.config:
+                libro_config.config['ipython_extensions'] = libro_config.config.pop("jpserver_extensions")
+        extensions = libro_config.get_config().get("ipython_extensions",{})
         commands = []
 
         # 遍历扩展，生成相应的 %load_ext 或 %unload_ext 命令
